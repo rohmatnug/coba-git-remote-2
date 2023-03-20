0a08d7afab1c81d94335d38b57fded36c6495fba
Rohmat Nugroho <rohmatnugroho99@gmail.com>
2023-03-20 21:19:06 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias amet qui harum facilis consequuntur quos illum quis, minus, quidem culpa tempora expedita odit ea praesentium impedit id aut vero. Similique.</p>    
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
